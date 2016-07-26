b16277681bc4968664229ddaa2b2c601c155e217
ttcat <ttcat0902@gmail.com>
2016-07-26 17:53:18 +0800

    html dom fix

--- scraper.php
+++ scraper.php
@@ -4,8 +4,9 @@
 
 require 'vendor/autoload.php';
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
+require 'vendor/openaustralia/scraperwiki/scraperwiki.php';
+require 'vendor/openaustralia/scraperwiki/simple_html_dom.php';
+
 //
 // Read in a page
 $html = scraperwiki::scrape("https://m.591.com.tw/mobile-list.html?type=rent8&version=1&o=31&regionid=3&sectionidStr=38&kind=8");
@@ -13,7 +14,30 @@ $html = scraperwiki::scrape("https://m.591.com.tw/mobile-list.html?type=rent8&ve
 // Find something on the page using css selectors
 $dom = new simple_html_dom();
 $dom->load($html);
-print_r($dom->find("data"));
+
+$row = (array) $dom->find(".data");
+
+// print_r($row);
+$new_row = array();
+
+foreach ($row as $k => $v) {
+	
+	$new_row[$k]['data-house'] = $v->attr['data-house'];
+	
+	$n1 = $v->find(".n1");
+	$n2 = $v->find(".n2");
+	$n3 = $v->find(".n3");
+	
+	$new_row[$k]['url'] = "https://m.591.com.tw/mobile-detail.html?type=rent&regionid=3&sectionidStr=38&price=&room=&area=&kind=8&shape=&firstRow=0&houseId=" .$v->attr['data-house']. "&hash=16";
+
+	$new_row[$k]['desc_1'] = $n1[0]->children[0]->nodes[0]->_[4];
+	$new_row[$k]['desc_2'] = $n2[0]->children[0]->parent->nodes[1]->_[4];
+	$new_row[$k]['desc_3'] = $n3[0]->nodes[0]->_[4];
+
+}
+
+
+print_r($new_row);
 
 
 //
